e982504beb066d89e91f227918b708f4198e2d15
Francois Janson <jansonfrancois@gmail.com>
2021-10-12 15:37:54 +0200

    cleaning code

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,8 @@ const {
     validationResult
 } = require("express-validator")
 const userModel = require("../models/userModel")
+const users = require("../users.json")
 
-const users = [{
-        username: "ever",
-        password: "12356",
-        city: "Paris"
-    },
-    {
-        username: "ever456",
-        password: "456899",
-        city: "Los Angeles"
-    },
-]
 
 const getSignupPage = (req, res) => {
     res.render("signup")
@@ -21,14 +11,19 @@ const getSignupPage = (req, res) => {
 const listUsers = (req, res) => {
     // const usersList = users.map(user => user.username)
     // res.json(usersList)
+
     userModel.find()
-        .then(data => res.json(data))
+        .then(users => {
+            const userList = users.map(user => user.username)
+            res.json(userList)
+        })
         .catch(err => res.json(err))
 }
 const showUser = (req, res) => {
     if (req.params.username) {
         // const user = users.find(user => user.username === req.params.username)
         // res.json(user)
+
         userModel.findOne({
                 username: req.params.username
             })
@@ -38,6 +33,7 @@ const showUser = (req, res) => {
     if (req.params.id) {
         // const user = users.find(user => user._id === req.params.id)
         // res.json(user)
+
         userModel.findOne({
                 _id: req.params.id
             })
@@ -55,14 +51,14 @@ const signUpUser = (req, res) => {
     }
     console.log("signupUser req.body", req.body)
     // users.push(req.body)
+    // res.send("User saved !")
     userModel.create({
             username: req.body.username,
             password: req.body.password,
             city: req.body.city
         })
-        .then(users => res.send("User saved !"))
+        .then(user => res.json(user))
         .catch(err => res.json(err))
-    res.send("User saved !")
 }
 
 module.exports = {
